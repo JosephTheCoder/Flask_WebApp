9342d65978e1a2ac75d091446eff901edbfad1be
JosephTheCoder <37300462+JosephTheCoder@users.noreply.github.com>
2018-06-09 16:32:21 +0100

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Config) #reads and applies the Config file
+db = SQLAlchemy(app) #db object represents the database
+migrate = Migrate(app, db) #represents the migration engine
 
-from app import routes
+from app import routes, models #models defines the structure of the database
